657d2c8a65d2dde0c7951238980fc1fade57efd3
Moritz Meißelbach <arbelzapf@gmail.com>
2021-08-23 20:23:27 +0200

    Add basic event dispatcher integration

--- factories.php
+++ factories.php
@@ -2,4 +2,29 @@
 
 declare(strict_types=1);
 
-return [];
+namespace Noem\Framework;
+
+use Noem\Container\Attribute\Tag;
+use Noem\Container\Attribute\Tagged;
+use Psr\EventDispatcher\EventDispatcherInterface;
+use Psr\EventDispatcher\ListenerProviderInterface;
+use Yiisoft\EventDispatcher\Dispatcher\Dispatcher;
+use Yiisoft\EventDispatcher\Provider\ListenerCollection;
+use Yiisoft\EventDispatcher\Provider\Provider;
+
+return [
+    'example-listener' =>
+        #[Tag('event-listener')]
+        fn() => function (\stdClass $event) {
+            echo __FILE__;
+        },
+    ListenerCollection::class => function (#[Tagged('event-listener')] callable ...$listeners) {
+        $collection = new ListenerCollection();
+        foreach ($listeners as $listener) {
+            $collection = $collection->add($listener);
+        }
+        return $collection;
+    },
+    ListenerProviderInterface::class => fn(ListenerCollection $l) => new Provider($l),
+    EventDispatcherInterface::class => fn(ListenerProviderInterface $p) => new Dispatcher($p)
+];
